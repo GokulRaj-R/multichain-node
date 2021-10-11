f984795a1124f8844855acb5d40e51bab5dafd56
Gokul Raj <gokul8262@gmail.com>
2021-10-11 06:43:49 +0000

    Update default record count and start

--- apis/stream.js
+++ apis/stream.js
@@ -3,102 +3,104 @@ const router = express.Router();
 
 const { rpcUser, rpcPass, rpcPort, rpcHost } = require("../configs/config");
 const mchain = require("multichain-node")({
-    port: rpcPort,
-    host: rpcHost,
-    user: rpcUser,
-    pass: rpcPass,
+	port: rpcPort,
+	host: rpcHost,
+	user: rpcUser,
+	pass: rpcPass,
 });
 
 router.get("/:stream", (req, res) => {
-    const { stream } = req.params;
-    let { start, count, verbose } = req.query;
+	const { stream } = req.params;
+	let { start, count, verbose } = req.query;
 
-    verbose = verbose === "true" ? true : false;
-    count = parseInt(count || 10);
-    start = parseInt(start || 0);
+	verbose = verbose === "true" ? true : false;
+	count = parseInt(count || 100);
+	start = parseInt(start || 32);
 
-    try {
-        mchain.listStreamItems(
-            {
-                stream: stream,
-                verbose: verbose,
-                count: count,
-                start: start,
-            },
-            (err, result) => {
-                if (err) {
-                    console.log(err);
-                    return res.status(400).json(err);
-                }
-                return res.json(result);
-            }
-        );
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json("Interval Server Error");
-    }
+	try {
+		mchain.listStreamItems(
+			{
+				stream: stream,
+				verbose: verbose,
+				count: count,
+				start: start,
+				'local-ordering': true
+			},
+			(err, result) => {
+				if (err) {
+					console.log(err);
+					return res.status(400).json(err);
+				}
+				const data = result.map(ele=> ({temp: ele.data.json.temp, time:ele.data.json.timestamp}))
+				return res.json(data);
+			}
+		);
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json("Interval Server Error");
+	}
 });
 
 router.get("/:stream/keys", (req, res) => {
-    const { stream } = req.params;
-    let { start, count, verbose } = req.query;
+	const { stream } = req.params;
+	let { start, count, verbose } = req.query;
 
-    verbose = verbose === "true" ? true : false;
-    count = parseInt(count || 10);
-    start = parseInt(start || 0);
+	verbose = verbose === "true" ? true : false;
+	count = parseInt(count || 10);
+	start = parseInt(start || 0);
 
-    try {
-        mchain.listStreamKeys(
-            {
-                stream: stream,
-                key: "*",
-                verbose: verbose,
-                count: count,
-                start: start,
-            },
-            (err, result) => {
-                if (err) {
-                    console.log(err);
-                    return res.status(400).json(err);
-                }
-                return res.json(result);
-            }
-        );
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json("Interval Server Error");
-    }
+	try {
+		mchain.listStreamKeys(
+			{
+				stream: stream,
+				key: "*",
+				verbose: verbose,
+				count: count,
+				start: start,
+			},
+			(err, result) => {
+				if (err) {
+					console.log(err);
+					return res.status(400).json(err);
+				}
+				return res.json(result);
+			}
+		);
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json("Interval Server Error");
+	}
 });
 
 router.get("/:stream/:key", (req, res) => {
-    const { stream, key } = req.params;
-    let { start, count, verbose } = req.query;
+	const { stream, key } = req.params;
+	let { start, count, verbose } = req.query;
 
-    verbose = verbose === "true" ? true : false;
-    count = parseInt(count || 10);
-    start = parseInt(start || 0);
+	verbose = verbose === "true" ? true : false;
+	count = parseInt(count || 10);
+	start = parseInt(start || 0);
 
-    try {
-        mchain.listStreamKeyItems(
-            {
-                stream: stream,
-                key: key,
-                verbose: verbose,
-                count: count,
-                start: start,
-            },
-            (err, result) => {
-                if (err) {
-                    console.log(err);
-                    return res.status(400).json(err);
-                }
-                return res.json(result);
-            }
-        );
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json("Interval Server Error");
-    }
+	try {
+		mchain.listStreamKeyItems(
+			{
+				stream: stream,
+				key: key,
+				verbose: verbose,
+				count: count,
+				start: start,
+			},
+			(err, result) => {
+				if (err) {
+					console.log(err);
+					return res.status(400).json(err);
+				}
+				return res.json(result);
+			}
+		);
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json("Interval Server Error");
+	}
 });
 
 module.exports = router;
